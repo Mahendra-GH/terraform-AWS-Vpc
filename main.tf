37470f30c61dd8642dad4c1dea832cdf2f31d560
Mahendra-GH <katammahendra20@gmail.com>
2024-09-20 14:07:37 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,43 +1,45 @@
 resource "aws_vpc" "base" {
-  cidr_block = "192.168.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
     Name = "From_Terraform"
   }
 
 }
-resource "aws_subnet" "One" {
+resource "aws_subnet" "subnets" {
+    count = 4
   vpc_id            = aws_vpc.base.id
   availability_zone = "ap-south-1a"
-  cidr_block        = "192.168.1.0/24"
+  cidr_block        = var.subnet_cidr_range[count.index]
   tags = {
-    Name = "web-01"
+    #count=4
+    Name = var.subnet_names[count.index]
   }
 
 }
-resource "aws_subnet" "two" {
-  vpc_id            = aws_vpc.base.id
-  availability_zone = "ap-south-1b"
-  cidr_block        = "192.168.2.0/24"
-  tags = {
-    Name = "web-02"
-  }
+# resource "aws_subnet" "two" {
+#   vpc_id            = aws_vpc.base.id
+#   availability_zone = "ap-south-1b"
+#   cidr_block        = var.subnet_cidr_range[1]
+#   tags = {
+#     Name = var.subnet_names[count.index]
+#   }
 
-}
-resource "aws_subnet" "three" {
-  vpc_id            = aws_vpc.base.id
-  availability_zone = "ap-south-1a"
-  cidr_block        = "192.168.3.0/24"
-  tags = {
-    Name = "app-01"
-  }
+# }
+# resource "aws_subnet" "three" {
+#   vpc_id            = aws_vpc.base.id
+#   availability_zone = "ap-south-1a"
+#   cidr_block        = var.subnet_cidr_range[2]
+#   tags = {
+#     Name = var.subnet_names[count.index]
+#   }
 
-}
-resource "aws_subnet" "four" {
-  vpc_id            = aws_vpc.base.id
-  availability_zone = "ap-south-1b"
-  cidr_block        = "192.168.4.0/24"
-  tags = {
-    Name = "app-2"
-  }
+# }
+# resource "aws_subnet" "four" {
+#   vpc_id            = aws_vpc.base.id
+#   availability_zone = "ap-south-1b"
+#   cidr_block        = var.subnet_cidr_range[3]
+#   tags = {
+#     Name = var.subnet_names[count.index]
+#   }
 
-}
+# }
